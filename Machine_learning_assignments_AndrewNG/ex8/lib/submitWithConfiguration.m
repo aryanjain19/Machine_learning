8217cad7cdab6913f7c71a063e80309bcb679c48
aryanjain19 <writetoaryan19@gmail.com>
2021-08-07 19:43:15 +0530

    Organise folders

--- Machine_learning_assignments_AndrewNG/ex8/lib/submitWithConfiguration.m
+++ Machine_learning_assignments_AndrewNG/ex8/lib/submitWithConfiguration.m
@@ -38,7 +38,7 @@ function submitWithConfiguration(conf)
   rmpath('./lib/jsonlab', './lib'); % Updated
 end
 
-function [email, token] = promptToken(email, existingToken, tokenFile)
+function [email token] = promptToken(email, existingToken, tokenFile)
   if (~isempty(email) && ~isempty(existingToken))
     prompt = sprintf( ...
       'Use token from last successful submission (%s)? (Y/n): ', ...
@@ -63,13 +63,36 @@ end
 function response = submitParts(conf, email, token, parts)
   body = makePostBody(conf, email, token, parts);
   submissionUrl = SubmissionUrl(); % Updated
-  params = {'jsonBody', body};
-  responseBody = urlread(submissionUrl, 'post', params);
+  responseBody = getResponse(submissionUrl, body);
   response = loadjson(responseBody);
 end
 
+function response = getResponse(url, body)
+  % NEW CURL SUBMISSION FOR WINDOWS AND MAC
+  if ispc
+    new_body = regexprep (body, '\"', '\\"'); % will escape double quoted objects to format properly for windows libcurl
+    json_command = sprintf('curl -X POST -H "Cache-Control: no-cache" -H "Content-Type: application/json" -d "%s" --ssl-no-revoke "%s"', new_body, url);
+    [code, response] = dos(json_command); %dos is for windows
+
+    new_response = regexp(response, '\{(.)*', 'match');
+    response = new_response{1,1};
+
+    % test the success code
+    if (code ~= 0)
+      fprintf('[error] submission with Invoke-WebRequest() was not successful\n');
+    end
+  else
+    json_command = sprintf('curl -X POST -H "Cache-Control: no-cache" -H "Content-Type: application/json" -d '' %s '' --ssl-no-revoke ''%s''', body, url);
+    [code, response] = system(json_command);
+    % test the success code
+    if (code ~= 0)
+      fprintf('[error] submission with curl() was not successful\n');
+    end
+  end
+end
+
 function body = makePostBody(conf, email, token, parts)
-  bodyStruct.assignmentSlug = conf.assignmentSlug;
+  bodyStruct.assignmentKey = conf.assignmentKey;
   bodyStruct.submitterEmail = email;
   bodyStruct.secret = token;
   bodyStruct.parts = makePartsStruct(conf, parts);
@@ -104,12 +127,13 @@ function showFeedback(parts, response)
   for part = parts
     score = '';
     partFeedback = '';
-    partFeedback = response.partFeedbacks.(makeValidFieldName(part{:}.id));
-    partEvaluation = response.partEvaluations.(makeValidFieldName(part{:}.id));
+    % NEW PARSING REPONSE BODY
+    partFeedback = response.linked.onDemandProgrammingScriptEvaluations_0x2E_v1{1}(1).parts.(makeValidFieldName(part{:}.id)).feedback;
+    partEvaluation = response.linked.onDemandProgrammingScriptEvaluations_0x2E_v1{1}(1).parts.(makeValidFieldName(part{:}.id));
     score = sprintf('%d / %3d', partEvaluation.score, partEvaluation.maxScore);
     fprintf('== %43s | %9s | %-s\n', part{:}.name, score, partFeedback);
   end
-  evaluation = response.evaluation;
+  evaluation = response.linked.onDemandProgrammingScriptEvaluations_0x2E_v1{1}(1);
   totalScore = sprintf('%d / %d', evaluation.score, evaluation.maxScore);
   fprintf('==                                   --------------------------------\n');
   fprintf('== %43s | %9s | %-s\n', '', totalScore, '');
@@ -122,5 +146,5 @@ end
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function submissionUrl = SubmissionUrl() % Updated
-  submissionUrl = 'https://www-origin.coursera.org/api/onDemandProgrammingImmediateFormSubmissions.v1';
+  submissionUrl = 'https://www.coursera.org/api/onDemandProgrammingScriptSubmissions.v1?includes=evaluation';
 end
--- README.md
+++ README.md
@@ -1,3 +1,15 @@
-# Data_Science
-This includes the data science models made by me till now.
+### Machine Learning
+I completed the Machine learning course by Andrew Ng (Stanford) through Coursera. 
+
+The certificate I received can be found **[here](https://coursera.org/share/5e6e24c6f4a867f51b5d31c82c63c0bc)**
+
+The **handwritten number recognition** dir contains a model which identifies the handwritten numbers.  
+
+The **exercise**  repo contains the assignments of that course
+The Course included- 
+  1. Linear regression
+  2. Classification
+  3. Neural networks 
+  4. Superwised learning
+  5. Unsuperwised learning
 
